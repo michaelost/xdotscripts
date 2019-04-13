b6c67f7c7c9da6ac2d17241697c95dc6978bb70c
michaelost <mikhaylo.ostroviy@gmail.com>
2019-04-13 15:09:56 +0300

    add example script and read

---
+++ README.md
@@ -0,0 +1,1 @@
+# This set of scripts is made for reducing repeatable monkey job (switching between windows, tabs, pressing some sequence of keys ect) during development. 
---
+++ example.sh
@@ -0,0 +1,10 @@
+sleep 1
+xdotool keydown ctrl key u
+xdotool keyup ctrl
+sleep 1
+
+xdotool key type ":q"
+xdotool key Return 
+xdotool key type "nvim opa2.js"
+xdotool key Return
+xdotool key type e
